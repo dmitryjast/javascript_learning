c2a0a731e1ce0cf2108962558b83ae180ad5a224
Dmitry <dmitry.jatsyshyn@gmail.com>
2019-09-16 22:22:29 +0300

    added 3st lesson

---
+++ JS3 lesson - main operators/js/script.js
@@ -0,0 +1,42 @@
+
+
+// 1. Unary operators - унарные операторы
+/*
++ - * /
+*/
+
+
+var a = '7';
+var b = 7;
+
+var result;
+// result = a % b;
+
+
+// 2. Variable increase +1 - увеличение переменной +1
+result = 3;
+//result++;
+// alert(result);
+
+
+// alert(--result);
+
+// alert(result);
+// result = result + 1;
+
+
+// 3. Code simplification - упрощение кода
+
+// result = result + 5;
+
+// верхняя строка по смыслу будет равна нижней
+
+// result += 5;
+
+// alert(result);
+
+
+
+// 4. Comparison - сравнение
+
+alert(a === b);
