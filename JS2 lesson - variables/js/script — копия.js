85e26cef89ae50add20173980037bdfdb8f19a57
Dmitry <dmitry.jatsyshyn@gmail.com>
2019-09-16 22:21:46 +0300

    added 2st lesson

---
+++ JS2 lesson - variables/js/script — копия.js
@@ -0,0 +1,34 @@
+
+
+
+
+var type;
+type = typeof myNumber;
+
+alert(type);
+
+
+// var myVariable = 5;
+
+// alert(myVariable);
+
+
+// Numbers - числа
+var myNumber = 5;
+var mySecondNumber = 5.154151455;
+
+// String - строки (текст)
+var str = 'Some text';
+var str = "Some text";
+
+// Boolean (переменная, которая может хранить 2 типа: true или false)
+var flag = true;
+var flag2 = false;
+
+// Null - позволяет указать значение для переменной, что пока значение неизвестно
+var bullVar = null;
+
+// undefined - тип, который позволяет указать перменной, что она неопределена пока что (присваевается переменным, которые не определены)
+var notDefinedVar = underfined
+
+
---
+++ JS2 lesson - variables/js/script.js
@@ -0,0 +1,25 @@
+
+// Numbers - числа
+// var myNumber = 5;
+// var mySecondNumber = 5.154151455;
+
+// String - строки (текст)
+var str = 'Some text';
+
+// Boolean (переменная, которая может хранить 2 типа: true или false)
+// var flag = true;
+// var flag2 = false;
+
+// Null - позволяет указать значение для переменной, что пока значение неизвестно
+// var nullVar = null;
+
+// undefined - тип, который позволяет указать перменной, что она неопределена пока что (присваевается переменным, которые не определены)
+// var notDefinedVar = underfined;
+
+// var someVariable;
+
+var type;
+
+type = typeof(str);
+
+alert(type);
