e1e32892da68aa193fd4ba9485e6e07761a1f974
Dmitry <dmitry.jatsyshyn@gmail.com>
2019-09-17 20:46:33 +0300

    added 5st lesson

---
+++ JS5 lesson - massives/js/script.js
@@ -0,0 +1,56 @@
+var arr = [1, true, '3', 4];
+
+// console.log('arr', arr);
+
+// Чтобы обратиться к какому-то элементу массива. 0 - это первый элемент, 1 - второй соответственно итд
+
+// console.log('arr', arr[1]);
+
+// console.log('arr', arr);
+
+// **Также существуют свойства массивов**
+// Чтобы увидеть длину массива
+
+// console.log('arr.length', arr.length);
+
+// Useful methods when working with arrays - полезные методы во время работы с массивами
+
+// arr.push('5');
+//
+// console.log('arr', arr);
+// console.log('arr.length', arr.length);
+//
+// arr.pop();
+//
+// console.log('arr', arr);
+// console.log('arr.length', arr.length);
+
+
+// Добавление элемента в конце массива
+
+// arr.push('5');
+// console.log("arr.push('5');");
+//
+// console.log('arr', arr);
+//
+// arr.pop();
+// console.log("arr.pop();");
+//
+// console.log('arr', arr);
+
+
+// Добавление элемента в начале массива
+
+// arr.unshift('5');
+// console.log("arr.unshift('5');");
+//
+// console.log('arr', arr);
+//
+// arr.pop();
+// console.log("arr.pop();");
+//
+// console.log('arr', arr);
+
+arr[10] = '10 element';
+
+console.log(arr);
