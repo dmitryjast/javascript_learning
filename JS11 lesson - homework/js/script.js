c79b0086221d97c0837913279f2611649ee4251a
Dmitry <dmitry.jatsyshyn@gmail.com>
2019-09-25 14:00:49 +0300

    added 11st homework - in progress

---
+++ JS11 lesson - homework/js/script.js
@@ -0,0 +1,34 @@
+
+
+function name(base, exponent) {
+	
+	var result = promt(description, default);
+	
+	добавить вывод окна для ввода цифр
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	var test = 'test';
+	
+	
+	
+	
+	
+	var result;
+	result = a + b + c;
+	alert('test');
+	return result
+}
